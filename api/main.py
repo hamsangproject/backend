d31495d63599c5f17cf2729e2e761680d1cd66ae
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2022-01-02 19:29:57 +0330

    clear unwanted

--- api/main.py
+++ api/main.py
@@ -12,11 +12,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
-
-
 @app.get("/api/v0/word/{word}")
 def get_term_from_all_dic(word: str):
     output = get_meaning(word, '*')
