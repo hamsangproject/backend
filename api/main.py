c615013d68f5529a9cbc3a3b5c0ac40c8da61688
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2022-01-17 01:32:57 +0330

    fix cors problem. is it fixed?

--- api/main.py
+++ api/main.py
@@ -1,9 +1,25 @@
 from typing import Optional
 from fastapi import FastAPI
 from core.dbmanager import get_meaning
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
@@ -22,3 +38,4 @@ def get_term_from_all_dic(word: str):
     }
 
     return result
+
