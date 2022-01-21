334ba89aff29c52d67fd8ecf85d3731bf1e619cf
Erfan Kheyrollahi Qaroglu <ekm507@gmail.com>
2022-01-21 22:53:10 +0330

    add *

--- api/main.py
+++ api/main.py
@@ -10,12 +10,15 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:8080",
+    "http://localhost:8000",
+    "https://localhost:8000",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origin,
+    # allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
